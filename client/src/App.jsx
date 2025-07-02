a79ae01d56d9d0ecd2077736e5b81bc625dae001
Numberwan08 <651413033@crru.ac.th>
2025-07-02 16:06:12 +0700

    add Product pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Dashboard from './admin/pages/dashboard'
 import Usermember from './admin/pages/Usermembers'
 import PostController from './admin/pages/PostControllers'
 import EventControllers from './admin/pages/EventControllers'
+import ProductControllers from './admin/pages/ProductControllers'
 
 function App() {
   const router = createBrowserRouter([
@@ -22,6 +23,10 @@ function App() {
     {
       path: '/admin/EventControllers',
       element: <EventControllers/>  ,
+    },
+    {
+      path: '/admin/ProductControllers',
+      element: <ProductControllers/>,
     }
   ])
   return (
---
+++ client/src/admin/pages/ProductControllers.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProductControllers() {
+  return (
+    <div>
+      ProductControllers
+    </div>
+  )
+}
+
+export default ProductControllers
