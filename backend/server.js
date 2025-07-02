e96c51544943c53b907f8fce880d8fc5404805d2
kajonsak002 <651463002@crru.ac.th>
2025-07-02 16:10:22 +0700

    add backend

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { readdirSync } = require("fs");
+
+dotenv.config();
+
+const app = express();
+const port = 3000 || process.env.PORT;
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Connected Api sucessfully" });
+});
+
+readdirSync("./routes").map((name) => {
+  try {
+    app.use("/api", require("./routes/" + name));
+  } catch (err) {
+    console.error("Error loading route file:", name, err.message);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Sever is runnind on port http://localhost:${port}`);
+});
