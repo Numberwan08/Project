e2ff68b1ac1d80df660f4d7eb21fe79f9aef29fe
Numberwan08 <651413033@crru.ac.th>
2025-07-26 23:02:26 +0700

    ล่าสุด

--- backend/controller/add_event.js
+++ backend/controller/add_event.js
@@ -7,19 +7,127 @@ exports.get_event = async (req , res )=> {
 }
 
 exports.add_event = async (req , res )=> {
-    res.json({
-        mag: "api เพิ่มกิจกรรม"
-    })
+    try{
+    console.log("req.bady",req.bady);
+        const {
+            id_user,
+            name_event,
+            location_event,
+            phone,
+            detail_event,
+            date,
+            images,
+            latitude,
+            longitude
+        }=req.body;
+
+        const [rows]=await db.promise().query("INSERT INTO user_event (id_user, name_event, location_event, phone, detail_event, date, images, latitude, longitude) VALUES (?,?,?,?,?,?,?,?,?)",[
+            id_user,
+            name_event,
+            location_event,
+            phone,
+            detail_event,
+            date,
+            images,
+            latitude,
+            longitude
+        ])
+
+        if(rows.affectedRows === 0) {
+            return res.status(400).json({
+                mag: "ไม่สามารถเพิ่มกิจกรรมได้",
+                error: "ไม่สามารถเพิ่มกิจกรรมได้"
+            })
+        }
+
+        return res.status(201).json({
+            mag: "เพิ่มกิจกรรมสำเร็จ"
+        })
+    }catch(err){
+        console.log("error add_event",err);
+        return res.status(500).json({
+            mag: "ไม่สามารถเพิ่มกิจกรรมได้",
+            error: err.message
+        })
+    }
 }
 
 exports.edit_event = async (req , res )=> {
-    res.json({
-        mag: "api แก้ไขกิจกรรม"
-    })
+    const {id} = req.params;
+    const {
+        name_event,
+        location_event,
+        phone,
+        detail_event,
+        date,
+        images,
+        latitude,
+        longitude
+    } = req.body;
+    try{
+
+        const [rows] = await db.promise().query("UPDATE user_event SET name_event = ?, location_event = ?, phone = ?, detail_event = ?, date = ?, images = ?, latitude = ?, longitude = ? WHERE id_event = ?", [
+            name_event,
+            location_event,
+            phone,
+            detail_event,
+            date,
+            images,
+            latitude,
+            longitude,
+            id
+        ]);
+
+        if(rows.affectedRows === 0) {
+            return res.status(400).json({
+                mag: "ไม่สามารถแก้ไขกิจกรรมได้",
+                error: "ไม่สามารถแก้ไขกิจกรรมได้"
+            });
+        }
+
+        return res.status(200).json({
+            mag: "แก้ไขกิจกรรมสำเร็จ"
+        });
+
+    }catch(err){
+        console.log("error edit_event",err);
+        return res.status(500).json({
+            mag: "ไม่สามารถแก้ไขกิจกรรมได้",
+            error: err.message
+        })
+    }
 }
 
 exports.delete_event = async (req , res )=> {
-    res.json({
-        mag: "api ลบกิจกรรม"
-    })
+    try{
+       
+        const {id} = req.params;
+
+        const [rows] =await db.promise().query("SELECT * FROM user_event WHERE id_event = ?",[id]);
+
+        if(rows.length === 0){
+            return res.status(404).json({
+                mag: "ไม่พบกิจกรรมที่ต้องการลบ",
+                error: "ไม่พบกิจกรรมที่ต้องการลบ"
+            })
+        }
+        const [result] = await db.promise().query("DELETE FROM user_event WHERE id_event = ?", [id]);
+        if(result.affectedRows === 0) {
+            return res.status(400).json({
+                mag: "ไม่สามารถลบกิจกรรมได้",
+                error: "ไม่สามารถลบกิจกรรมได้"
+            });
+        }
+        return res.status(200).json({
+            mag: "ลบกิจกรรมสำเร็จ"
+        });
+
+    } catch(err){
+        console.log("error delete_event",err);
+        return res.status(500).json({
+            mag: "ไม่สามารถลบกิจกรรมได้",
+            error: err.message
+        })
+    }
+   
 }
--- backend/controller/add_prodact.js
+++ backend/controller/add_prodact.js
@@ -7,19 +7,123 @@ exports.get_prodact = async (req , res )=>{
 }
 
 exports.add_prodact = async (req , res )=>{
-    res.json({
-        mag: "api เพิ่มสินค้า"
-    })
+    try {
+        console.log("req.body", req.body);
+        const {
+            id_user,
+            name_prodact,
+            detail_prodact,
+            date,
+            phone,
+            latitude,
+            longitude,
+            price,
+            images
+        }= req.body;
+        const [rows] = await db.promise().query("INSERT INTO user_prodact (id_user, name_prodact, detail_prodact, date, phone, latitude, longitude, price, images) VALUES (?,?,?,?,?,?,?,?,?)", [
+            id_user,
+            name_prodact,
+            detail_prodact,
+            date,
+            phone,
+            latitude,
+            longitude,
+            price,
+            images
+        ]);
+
+        if(rows.affectedRows === 0) {
+            return res.status(400).json({
+                msg: "ไม่สามารถเพิ่มสินค้าได้",
+                error: "ไม่สามารถเพิ่มสินค้าได้"
+            });
+        }
+        return res.status(201).json({
+            msg: "เพิ่มสินค้าสำเร็จ"
+        });
+
+
+    }catch(err) {
+        console.log("error add_prodact", err);
+        return res.status(500).json({
+            msg: "ไม่สามารถเพิ่มสินค้าได้",
+            error: err.message
+        });
+    }
 }
 
 exports.edit_prodact = async (req , res )=>{
-    res.json({
-        mag: "api แก้ไขสินค้า"
-    })
+    const {id} = req.params;
+    const {
+        name_prodact,
+        detail_prodact,
+        date,
+        phone,
+        latitude,
+        longitude,
+        price,
+        images
+    } = req.body;
+    try{
+        
+        const [rows] = await db.promise().query("UPDATE user_prodact SET name_prodact = ?, detail_prodact = ?, date = ?, phone = ?, latitude = ?, longitude = ?, price = ?, images = ? WHERE id_prodact = ?", [
+            name_prodact,
+            detail_prodact,
+            date,
+            phone,
+            latitude,
+            longitude,
+            price,
+            images,
+            id
+        ]);
+
+        if(rows.affectedRows === 0) {
+            return res.status(400).json({
+                msg: "ไม่สามารถแก้ไขสินค้าได้",
+                error: "ไม่สามารถแก้ไขสินค้าได้"
+            });
+        }
+        return res.status(200).json({
+            msg: "แก้ไขสินค้าสำเร็จ"
+        });
+
+    } catch(err) {
+        console.log("error edit_prodact", err);
+        return res.status(500).json({
+            msg: "ไม่สามารถแก้ไขสินค้าได้",
+            error: err.message
+        });
+    }
 }
 
 exports.delete_prodact = async (req , res )=>{
-    res.json({
-        mag: "api ลบสินค้า"
-    })
+    try {
+        const {id} = req.params;
+
+        const [rows] =await db.promise().query("SELECT * FROM user_prodact WHERE id_prodact = ?", [id]);
+        if(rows.length === 0) {
+            return res.status(404).json({
+                msg: "ไม่พบสินค้าที่ต้องการลบ",
+                error: "ไม่พบสินค้าที่ต้องการลบ"
+            });
+        }
+        await db.promise().query("DELETE FROM user_prodact WHERE id_prodact = ?", [id]);
+        if(rows.affectedRows === 0) {
+            return res.status(400).json({
+                msg: "ไม่สามารถลบสินค้าได้",
+                error: "ไม่สามารถลบสินค้าได้"
+            });
+        }
+        return res.status(200).json({
+            msg: "ลบสินค้าสำเร็จ"
+        });
+
+    } catch(err) {
+        console.log("error delete_prodact", err);
+        return res.status(500).json({
+            msg: "ไม่สามารถลบสินค้าได้",
+            error: err.message
+        });
+    }
 }
